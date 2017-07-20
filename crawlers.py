d3bf0daa06de71968c6be2deaedcad8fe19aff0c
andrewisakov <andrew.isakov@gmail.com>
2017-07-20 17:01:38 +0300

    п.1

--- crawlers.py
+++ crawlers.py
@@ -1,7 +1,31 @@
+#!/usr/bin/python3
 import datetime
 import time
 import settings
 
+
 class Crawler:
     def __init__(self):
-        self.db = settings.DB
+        db = settings.DB
+        c = db.cursor()
+        c.execute('select s.name, s.id '
+                  'from Sites s '
+                  'left join Pages p on (p.SiteID=s.ID) '
+                  'where p.id is Null')
+        INSERT = 'insert into Pages(SiteID, Url) values (?, ?)'
+        ARGS = [(r[1], '%s/robots.txt' % r[0]) for r in c.fetchall()]
+        c.close()
+        c = db.cursor()
+        try:
+            print(ARGS)
+            c.executemany(INSERT, ARGS)
+            db.commit()
+        except Exception as e:
+            db.rollback()
+            print('Crawler exception', e)
+
+        print('Crawler init done ...')
+
+
+if __name__ == '__main__':
+    c = Crawler()
