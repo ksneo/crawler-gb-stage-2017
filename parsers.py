ef98e59633542da5d35ea0130fb736d46d20450b
andrewisakov <andrew.isakov@gmail.com>
2017-07-28 16:27:26 +0300

    разборки с html_parser

--- parsers.py
+++ parsers.py
@@ -6,28 +6,35 @@ from lxml import etree
 from collections import Counter
 from log import log_with
 
+
 def _count_words(words_list, words_dict):
     """
         words_list - список слов для поиска и подсчета
         words_dict - словарь {"person_id": [person_words]}
         возвращает словарь {"person_id": "person_rank"}
     """
+    print('_count_words %s started...' % len(words_list))
     c = Counter(words_list)
     result = {}
     for person_id, person_words in words_dict.items():
         pr_sum = sum([c[word] for word in person_words])
         result[person_id] = pr_sum
+
+    print('_count_words %s completed...' % len(words_list))
     return result
 
 
 def _extract_text(page_content):
     """
-        page_content(html) -> html_text текст без тегов 
+        page_content(html) -> html_text текст без тегов
     """
+    print('_extract_text %s started...' % len(page_content))
     parser = etree.HTMLParser()
     html_body = etree.parse(io.BytesIO(page_content.encode()), parser)
     html_text = ' '.join([text.strip() for text in html_body.xpath('body//*/text()')])
+    print('_extract_text %s complete...' % len(html_text))
     return html_text
+
 
 @log_with
 def _split_text(page_text, min_len):
@@ -36,6 +43,7 @@ def _split_text(page_text, min_len):
         page_text - текст страницы
         над регуляркой можно поработать, это самая простая и вроде дает адекватный результат
     """
+    print('_split_text', len(page_text), min_len)
     return [word.lower() for word in re.split(r'\W+', page_text) if len(word) > min_len]
 
 
@@ -47,12 +55,13 @@ def parse_html(page_content, words_dict):
     """
     min_len = 3 # минимальная длина слова которе считается словом
     result = {}
-    print('parse_html %s in %s' % (words_dict, len(page_content)))
+    print('parse_html %s in %s started ...' % (words_dict, len(page_content)))
     try:
         html_text = _extract_text(page_content)
         words_list = _split_text(html_text, min_len)
         result = _count_words(words_list, words_dict)
     except Exception as ex:
         logging.error("parsers.parse_html: error %s", ex)
+    print('parse_html %s completed...' % result)
 
     return result
