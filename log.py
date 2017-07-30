e7cb5320044159b89df2ea35ee810669d7df1a01
ksneo <vkashapov@gmail.com>
2017-07-30 09:49:03 +0300

    Add setup of logging level from settings

--- log.py
+++ log.py
@@ -1,13 +1,26 @@
 import logging
+import settings
 
+log_file = settings.LOG_FILE if settings.LOG_FILE else 'crawler.log'
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.INFO)
-handler = logging.FileHandler('crawler.log', 'w', 'utf-8')
-root_logger.addHandler(handler)
+
+if settings.DEBUG:
+    root_logger.setLevel(logging.DEBUG)
+else:
+    root_logger.setLevel(logging.INFO)
+log_handler = logging.FileHandler(log_file, 'w', 'utf-8')
+root_logger.addHandler(log_handler)
 
 def log_with(func):
     def wrapper(self, *argv, **kwargv):
         result = func(self, *argv, **kwargv)
         logging.info("%s , result: %s", func.__name__, result)
+        return result
+    return wrapper
+
+def debug_log(func):
+    def wrapper(self, *argv, **kwargv):
+        result = func(self, *argv, **kwargv)
+        logging.debug("%s , result: %s", func.__name__, result)
         return result
     return wrapper
