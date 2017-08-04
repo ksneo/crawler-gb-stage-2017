6047cad2723b94558c89fc828a54e37f487359ed
andrewisakov <andrew.isakov@gmail.com>
2017-08-04 19:01:20 +0300

    заработало

--- crawlers.py
+++ crawlers.py
@@ -59,7 +59,7 @@ def scan_page(page, all_robots):
 
 def scan(next_step=False, max_limit=0):
     urls_limits = {}
-    pool_size = 0
+    pool_size = [0, ]
 
     def get_content_complete(*args):
         content, page, all_robots = args[0]
@@ -68,37 +68,37 @@ def scan(next_step=False, max_limit=0):
             urls_limits[site_id] = 0
         urls_limits[site_id] += 1
         # print('get_content_complete: %s/%s' % (urls_limits[site_id], max_limit), page)
-        pool_size -= 1
+        pool_size[0] -= 1
         if (max_limit == 0) or (urls_limits[site_id] < max_limit):
             print('get_content_complete continue: %s/%s' % (urls_limits[site_id], max_limit), page)
             robots = all_robots.get(site_id)
-            pool_size += 1
+            pool_size[0] += 1
             pool.apply_async(sitemap.scan_urls, (content, page, robots,),
                              callback=scan_page_complete, error_callback=scan_error)
-            pool_size += 1
+            pool_size[0] += 1
             pool.apply_async(parsers.process_ranks, (content, page_id, keywords,),
                              callback=process_ranks_complete, error_callback=scan_error)
 
     def process_ranks_complete(ranks):
-        pool_size -= 1
+        pool_size[0] -= 1
         print('process_ranks_complete:', ranks)
 
     def scan_page_complete(*args):
         """Страничная запись url'ов в БД"""
-        pool_size -= 1
+        pool_size[0] -= 1
         print('scan_page_complete:', args[0])
         new_pages_data, page_id, page_type = args[0]
 
         # if page_type == sitemap.SM_TYPE_HTML:
         #     parsers.process_ranks(content, page_id)
         for r in range(0, len(new_pages_data), settings.CHUNK_SIZE):
-            pool_size += 1
+            pool_size[0] += 1
             pool.apply_async(database.add_urls,
                      (new_pages_data[r:r+settings.CHUNK_SIZE], page_id, page_type,))
 
     def scan_error(error):
-        pool_size -= 1
-        logging.error('scan_error: %s', pool_size, error)
+        pool_size[0] -= 1
+        logging.error('scan_error: %s -- %s' % (pool_size[0], error))
 
     all_robots = robots.process_robots()
     keywords = database.load_persons()
@@ -111,14 +111,14 @@ def scan(next_step=False, max_limit=0):
     add_urls_total = 0
     # scans = []
     for page in database.get_pages_rows(max_limit=max_limit):
-        pool_size += 1
+        pool_size[0] += 1
         pool.apply_async(get_content_mp, (page, all_robots),
                             callback=get_content_complete, error_callback=scan_error)
         while pool._taskqueue.qsize() > settings.WORK_LIMIT:
             print(pool._taskqueue.qsize())
             time.sleep(settings.WORK_TIMEOUT)
 
-    while pool_size > 0:
+    while pool_size[0] > 0:
         # Ожидание опустошения пула
         time.sleep(1)
         print('pool.qsize:', pool_size, pool._taskqueue.qsize())
